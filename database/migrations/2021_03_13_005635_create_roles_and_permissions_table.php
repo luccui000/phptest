8d7c2916a85d5922b2d49f503e0d72ededf5bd96
luccui <luccui2k@gmail.com>
2021-03-13 08:08:49 +0700

    them phan quyen database trong he thong

---
+++ database/migrations/2021_03_13_005635_create_roles_and_permissions_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesAndPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
+        });
+        Schema::create('role_permission', function(Blueprint $table) {
+            $table->unsignedInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->unsignedInteger('permission_id');
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+            $table->primary(['role_id', 'permission_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('permissions');
+        Schema::table('role_permission', function (Blueprint $table) {
+            $table->dropForeign('role_id');
+            $table->dropForeign('permission_id');
+        });
+        
+    }
+}
